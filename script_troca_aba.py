759543a5053404633f71e6587e9f641d3dcd8c6e
hectorcrizel <146693049+hectorcrizel@users.noreply.github.com>
2024-04-25 10:08:35 -0300

    Update script_troca_aba.py

--- script_troca_aba.py
+++ script_troca_aba.py
@@ -30,7 +30,7 @@ while True:
         while True:
             WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
             start_time = time.time()
-            while time.time() - start_time < 4:
+            while time.time() - start_time < 10:
                 if check_pause():
                     pause = not pause
                     time.sleep(0.2)
