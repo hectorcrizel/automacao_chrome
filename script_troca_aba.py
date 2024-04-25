bfe88b09144510c5aa0630ec5704b1de929435a7
surf2008 <146693049+surf2008@users.noreply.github.com>
2024-04-25 09:32:05 -0300

    Create script_troca_aba.py

---
+++ script_troca_aba.py
@@ -0,0 +1,47 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import keyboard
+
+
+driver = webdriver.Chrome()
+
+# Configura abas a serem abertas e automatizadas
+
+driver.get("https://www.urlteste.com")
+driver.execute_script("window.open('https://www.urlteste.org');")
+driver.execute_script("window.open('https://www.urlteste.com');")
+driver.execute_script("window.open('https://www.urlteste.com');")
+
+handles = driver.window_handles
+
+pause = False
+
+def check_pause():
+    return keyboard.is_pressed('ctrl') and keyboard.is_pressed('space')
+
+while True:
+    for handle in handles:
+        driver.switch_to.window(handle)
+        driver.refresh()
+        while True:
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+            start_time = time.time()
+            while time.time() - start_time < 4:
+                if check_pause():
+                    pause = not pause
+                    time.sleep(0.2)
+                    break
+                if pause:
+                    time.sleep(0.1)
+                    continue
+                time.sleep(0.1)
+            if pause:
+                continue
+            break
+    if pause:
+        break
+
+driver.quit()
